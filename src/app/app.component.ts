5a7b2563c0d45b06099c9618386a2a28a1fab3a0
Andrei Ivanovici <andrei.ivanovici@gmail.com>
2020-03-28 13:28:33 +0200

    feat(rovid) initial implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'covid-ro';
+export class AppComponent implements OnInit {
+  public data;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.data = this.httpClient.get('https://pomber.github.io/covid19/timeseries.json')
+      .pipe(map(data => data['Romania']));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {AppHeaderComponent} from './app-header/app-header.component';
+import {CovidDataChartComponent} from './covid-data-chart/covid-data-chart.component';
+import {NgxChartsModule} from '@swimlane/ngx-charts';
+import {HttpClientModule} from '@angular/common/http';
+import { CovidDataComponent } from './covid-data/covid-data.component';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    CovidDataChartComponent,
+    CovidDataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    HttpClientModule,
+    NgxChartsModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
